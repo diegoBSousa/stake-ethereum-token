20953b2fb4d4fd57da4862ba894203ad0481689f
Diego Sousa <diego@diegosousa.com.br>
2021-11-29 10:42:27 -0300

    Farming Token Test

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -51,4 +51,58 @@ contract('TokenFarm', ([owner, investor]) => {
       assert.equal(balance.toString(), convertTokenToWei('1000000'))
     })
   })
+
+  describe('Farming tokens', async () => {
+    it('rewards investors for staking mDai tokens', async () => {
+      let result
+
+      // Check investor balance before staking
+      result = await daiToken.balanceOf(investor)
+      assert.equal(
+        result.toString(),
+        convertTokenToWei('100'),
+        'investor Mock DAI wallet balance correct before staking'
+      )
+
+      // Stake Mock DAI Tokens
+      await daiToken.approve(
+        tokenFarm.address,
+        convertTokenToWei('100'),
+        {
+          from: investor
+        }
+      )
+
+      await tokenFarm.stakeTokens(convertTokenToWei('100'), { from: investor })
+
+      // Check staking result
+      result = await daiToken.balanceOf(investor)
+      assert.equal(
+        result.toString(),
+        convertTokenToWei('0'),
+        'investor Mock DAI wallet balance correct after staking'
+      )
+
+      result = await daiToken.balanceOf(tokenFarm.address)
+      assert.equal(
+        result.toString(),
+        convertTokenToWei('100'),
+        'Token Farm Mock DAI balance correct after staking'
+      )
+
+      result = await tokenFarm.stakingBalance(investor)
+      assert.equal(
+        result.toString(),
+        convertTokenToWei('100'),
+        'Investor staking balance correct after staking'
+      )
+
+      result = await tokenFarm.isStaking(investor)
+      assert.equal(
+        result.toString(),
+        'true',
+        'investor staking status correct after staking'
+      )
+    })
+  })
 })
